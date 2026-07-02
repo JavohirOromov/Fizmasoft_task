8c8a36b0c6cbcfd3f6882bff4321261eaef790ea
Javohir <oromovjavoxir@gmail.com>
2026-07-02 15:43:30 +0500

    SplashScreen.kt added

--- app/src/main/java/com/javohir/fizmasofttask/MainActivity.kt
+++ app/src/main/java/com/javohir/fizmasofttask/MainActivity.kt
@@ -5,14 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.javohir.fizmasofttask.core.ui.FizmasoftTaskTheme
