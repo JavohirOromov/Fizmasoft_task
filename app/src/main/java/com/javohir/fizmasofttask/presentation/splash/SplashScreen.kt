f55508a02a9624dcaa6e884cb0e1518bdd05b131
Javohir <oromovjavoxir@gmail.com>
2026-07-02 15:43:42 +0500

    SplashScreen.kt added

--- app/src/main/java/com/javohir/fizmasofttask/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/javohir/fizmasofttask/presentation/splash/SplashScreen.kt
@@ -16,13 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
