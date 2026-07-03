e1466f855758a54239b6e2478c7eab4e7cb12ef4
Javohir <oromovjavoxir@gmail.com>
2026-07-04 00:42:26 +0500

    Shortcomings have been corrected.

--- app/src/main/java/com/javohir/fizmasofttask/presentation/main/prayer/PrayerScreen.kt
+++ app/src/main/java/com/javohir/fizmasofttask/presentation/main/prayer/PrayerScreen.kt
@@ -2,7 +2,11 @@ package com.javohir.fizmasofttask.presentation.main.prayer
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.os.PowerManager
+import android.provider.Settings
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -30,6 +34,7 @@ import com.javohir.fizmasofttask.core.ui.Background
 import com.javohir.fizmasofttask.core.ui.TextSecondary
 import com.javohir.fizmasofttask.presentation.main.prayer.component.HeaderCard
 import com.javohir.fizmasofttask.presentation.main.prayer.component.PrayerRow
+import androidx.core.net.toUri
 
 /**
  * Created by: Javohir Oromov macos
@@ -37,7 +42,7 @@ import com.javohir.fizmasofttask.presentation.main.prayer.component.PrayerRow
  * Package: com.javohir.fizmasofttask.presentation.main.prayer
  * Description: Namoz vaqtlari tab
  */
-@SuppressLint("InlinedApi")
+@SuppressLint("InlinedApi", "BatteryLife")
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun PrayerScreen(
